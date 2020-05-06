b72c41bb7124d25f1879b0d4b2661a0e2207e064
Sergio <sborrelli@icloud.com>
2020-05-06 08:14:40 +0100

    Working on testing navigation

--- app/navigation/__tests__/PostsStackMain.spec.js
+++ app/navigation/__tests__/PostsStackMain.spec.js
@@ -2,9 +2,16 @@
 import React from 'react';
 import { renderWithRedux } from 'test-utils';
 import { NavigationContainer } from '@react-navigation/native';
-import { render, fireEvent, cleanup } from 'react-native-testing-library';
+import {
+  render,
+  fireEvent,
+  cleanup,
+  waitForElement,
+} from 'react-native-testing-library';
 import PostsStackMain from '../PostsStackMain';
+import TabNavigator from '../TabNavigator';
 import LoginForm from '../../components/login/login-form/LoginForm';
+import SigningStackNavigator from '../SigningStackNavigator';
 
 // Silence the warning https://github.com/facebook/react-native/issues/11094#issuecomment-263240420
 jest.mock('react-native/Libraries/Animated/src/NativeAnimatedHelper');
@@ -18,17 +25,39 @@ jest.mock('react-native-image-picker', () => ({
 describe('Testing react navigation', () => {
   afterEach(cleanup);
 
-  test('page contains the header and 10 items', () => {
-    const { getByText, getAllByText, debug } = renderWithRedux(
-      <NavigationContainer>
-        <PostsStackMain />
-      </NavigationContainer>,
+  test('page contains the header and 10 items', async () => {
+    const {
+      getByText,
+      getAllByText,
+      getByTestId,
+      debug,
+      getByPlaceholder,
+      toJSON,
+    } = renderWithRedux(<SigningStackNavigator />);
+
+    // debug()
+
+    // const signInHeader = getAllByText('LOG IN');
+    // expect(signInHeader).toBeTruthy();
+
+    const goToSignUpButton = getByTestId('switch-btn');
+    fireEvent.press(goToSignUpButton);
+    const signUpHeader = getAllByText('SIGN UP');
+    expect(signUpHeader).toBeTruthy();
+
+    /*const userNameField = await waitForElement(() =>
+      getByPlaceholder('Confirm Password'),
     );
+    expect(userNameField).toBeTruthy();*/
 
-    debug()
-
-    // const header = getByText('List of numbers from 1 to 20');
-    // const items = getAllByText(/Item number/);
+    // debug();
+    // const addPostButton = getByText('+');
+    // fireEvent.press(addPostButton);
+    // const headerAddNewPost = getAllByText('NEW POST');
+    // expect(headerAddNewPost).toBeTruthy();
+    // debug();
+    // const itemsRight = getAllByText('Post');
+    // expect(itemsRight).toBeTruthy();
 
     // expect(header).toBeTruthy();
     // expect(items.length).toBe(10);
